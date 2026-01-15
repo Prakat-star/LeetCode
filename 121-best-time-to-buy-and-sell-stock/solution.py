1a3f4d14505aa0dc4f52e585b494a2fe5e312fae
Prakat <prakatsharma064@gmail.com>
2026-01-15 11:38:42 +0545

    Optimize maxProfit method for better performance

--- 121-best-time-to-buy-and-sell-stock/solution.py
+++ 121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
+        min_price = prices[0]
 
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                profit = prices[j] - prices[i]
-                max_profit = max(max_profit, profit)
+        for i in range(1, len(prices)):
+            profit = prices[i] - min_price
+            max_profit = max(max_profit, profit)
+            min_price = min(min_price, prices[i])
 
         return max_profit
