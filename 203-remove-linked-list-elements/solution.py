038f75be5fa298501175375f17a823d2e8a6725d
Prakat Sharma <prakatsharma064@gmail.com>
2026-02-02 18:26:23 +0545

    move zeros

--- 203-remove-linked-list-elements/solution.py
+++ 203-remove-linked-list-elements/solution.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+    def removeElements(self, head, val: int):
         while head and head.val == val:
             head = head.next
 
